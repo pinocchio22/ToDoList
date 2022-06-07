31160f3c9ae86a5b75041ce0abbcb765b4670c42
pinocchio22 <vviian.2@gmail.com>
2022-06-07 18:39:47 +0900

    item

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.ToDoDao
+import com.example.todolist.db.ToDoEntity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db : AppDatabase
+    private lateinit var todoDao: ToDoDao
+    private lateinit var todoList : ArrayList<ToDoEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +24,21 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddTodoActivity::class.java)
             startActivity(intent)
         }
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        getAllTodoList()
+    }
+
+    private fun getAllTodoList() {
+        Thread {
+            todoList = ArrayList(todoDao.getAll())
+            setRecyclerView()
+        }.start()
+    }
+
+    private fun setRecyclerView() {
+        // 리사이클러뷰
     }
 }
