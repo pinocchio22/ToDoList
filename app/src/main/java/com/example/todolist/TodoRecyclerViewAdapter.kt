0d9003811a4b5d5c025d757b4ec273371ab07d6a
pinocchio22 <vviian.2@gmail.com>
2022-06-08 16:05:57 +0900

    TodoRecyclerViewAdapter

---
+++ app/src/main/java/com/example/todolist/TodoRecyclerViewAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ItemTodoBinding
+import com.example.todolist.db.ToDoEntity
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2022-06-08
+ * @desc
+ */
+class TodoRecyclerViewAdapter(private val todoList : ArrayList<ToDoEntity>) : RecyclerView.Adapter<TodoRecyclerViewAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(binding : ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
+        val tv_importance = binding.tvImportance
+        val tv_title = binding.tvTitle
+        val root = binding.root
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding : ItemTodoBinding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val todoData = todoList[position]
+
+        when (todoData.importance) {
+            1 -> holder.tv_importance.setBackgroundResource(R.color.red)
+            2 -> holder.tv_importance.setBackgroundResource(R.color.yellow)
+            3 -> holder.tv_importance.setBackgroundResource(R.color.green)
+        }
+        holder.tv_importance.text = todoData.importance.toString()
+        holder.tv_title.text = todoData.title
+    }
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+
+
+}
